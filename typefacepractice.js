581355220e40f879419932d1ac82e6d9cc2d5997
WalkerL2000 <walker.lambert@cca.edu>
2021-02-24 18:29:16 -0800

    b4

---
+++ typefacepractice.js
@@ -0,0 +1,54 @@
+
+
+
+$(".1").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active1');
+});
+
+$(".2").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active2');
+});
+
+$(".3").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active3');
+});
+
+$(".4").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active4');
+});
+
+$(".5").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active5');
+});
+
+$(".6").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active6');
+});
+
+$(".7").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active7');
+});
+
+$(".8").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active8');
+});
+
+$(".9").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active9');
+});
+
+$(".10").on('mouseover', function() {
+  $(".circle").removeClass('active');
+  $(this).addClass('active10');
+});
+
+
